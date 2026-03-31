b8cdb94891126b246f430f716d54d3c0a9c0ae95
Allan Prado <allanp.vba@gmail.com>
2026-03-31 20:52:56 -0300

    refactor(games): update Round to use ProvablyFair value object

--- services/games/src/domain/entities/round.ts
+++ services/games/src/domain/entities/round.ts
@@ -1,13 +1,12 @@
 import { RoundStatus } from "../enums";
 import type { Optional } from "../types";
+import { ProvablyFair } from "../value-objects";
 import { Entity } from "./entity";
 import type { UniqueEntityId } from "./unique-entity-id";
 
 export interface RoundProps {
   status: RoundStatus;
-  serverSeed: string;
-  serverSeedHash: string;
-  crashPoint: number;
+  provablyFair: ProvablyFair;
   startedAt: Date;
   endedAt: Date | null;
   crashedAt: Date | null;
@@ -20,16 +19,26 @@ export class Round extends Entity<RoundProps> {
     return this.props.status;
   }
 
+  get provablyFair(): ProvablyFair {
+    return this.props.provablyFair;
+  }
+
   static create(
     props: Optional<
       RoundProps,
-      "createdAt" | "updatedAt" | "endedAt" | "crashedAt" | "status"
+      | "createdAt"
+      | "updatedAt"
+      | "endedAt"
+      | "crashedAt"
+      | "status"
+      | "provablyFair"
     >,
     id?: UniqueEntityId
   ): Round {
     return new Round(
       {
         status: RoundStatus.BETTING,
+        provablyFair: props.provablyFair ?? ProvablyFair.generate(),
         endedAt: null,
         crashedAt: null,
         ...props,
