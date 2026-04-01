13393aff7b5b61cd1ee983b74179f1cd79438781
Allan Prado <allanp.vba@gmail.com>
2026-04-01 14:00:29 -0300

    refactor(games): move EventEmitterModule.forRoot to AppModule
    forRoot() should be called in root module. Child modules
    get EventEmitter2 injected without importing the module.

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { EventEmitterModule } from "@nestjs/event-emitter";
 import { EnvModule } from "./infrastructure/env/env.module";
 import { GameEngineModule } from "./infrastructure/game-engine/game-engine.module";
 import { GamesController } from "./presentation/controllers/games.controller";
 
 @Module({
-  imports: [EnvModule, GameEngineModule],
+  imports: [EnvModule, EventEmitterModule.forRoot(), GameEngineModule],
   controllers: [GamesController]
 })
 export class AppModule {}
--- services/games/src/infrastructure/game-engine/game-engine.module.ts
+++ services/games/src/infrastructure/game-engine/game-engine.module.ts
@@ -1,10 +1,9 @@
 import { UseCasesModule } from "@/application/use-cases/use-cases.module";
 import { Module } from "@nestjs/common";
-import { EventEmitterModule } from "@nestjs/event-emitter";
 import { GameEngineService } from "./game-engine.service";
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), UseCasesModule],
+  imports: [UseCasesModule],
   providers: [GameEngineService],
   exports: [GameEngineService]
 })
