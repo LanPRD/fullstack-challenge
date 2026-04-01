9b04f89935f5e768e0802fe1884b8e2357a1350a
Allan Prado <allanp.vba@gmail.com>
2026-04-01 13:44:42 -0300

    refactor(games): simplify GameEngineModule
    Import UseCasesModule instead of manually providing
    repositories. This follows proper NestJS module composition.

--- services/games/src/infrastructure/game-engine/game-engine.module.ts
+++ services/games/src/infrastructure/game-engine/game-engine.module.ts
@@ -1,17 +1,11 @@
-import { RoundRepository } from "@/domain";
+import { UseCasesModule } from "@/application/use-cases/use-cases.module";
 import { Module } from "@nestjs/common";
 import { EventEmitterModule } from "@nestjs/event-emitter";
 import { GameEngineService } from "./game-engine.service";
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
-  providers: [
-    GameEngineService,
-    {
-      provide: RoundRepository,
-      useValue: {} // Will be replaced by actual implementation
-    }
-  ],
+  imports: [EventEmitterModule.forRoot(), UseCasesModule],
+  providers: [GameEngineService],
   exports: [GameEngineService]
 })
 export class GameEngineModule {}
