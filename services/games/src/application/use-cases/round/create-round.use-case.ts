551b3ff44d117c89666e11b797bbb659c0785deb
Allan Prado <allanp.vba@gmail.com>
2026-04-01 13:31:02 -0300

    feat(games): add CreateRound use case with barrel export

---
+++ services/games/src/application/use-cases/round/create-round.use-case.ts
@@ -0,0 +1,30 @@
+import { InternalException } from "@/application/errors";
+import {
+  Round,
+  left,
+  right,
+  type Either,
+  type RoundRepository
+} from "@/domain";
+import { Injectable, Logger } from "@nestjs/common";
+
+type CreateRoundOutput = Either<InternalException, Round>;
+
+@Injectable()
+export class CreateRoundUseCase {
+  private readonly _logger = new Logger(CreateRoundUseCase.name);
+
+  constructor(private readonly roundRepository: RoundRepository) {}
+
+  async execute(): Promise<CreateRoundOutput> {
+    const round = Round.create({ startedAt: new Date() });
+
+    try {
+      await this.roundRepository.save(round);
+      return right(round);
+    } catch (error) {
+      this._logger.error(`Error creating round: ${error}`);
+      return left(new InternalException({ message: "Error creating round" }));
+    }
+  }
+}
---
+++ services/games/src/application/use-cases/round/index.ts
@@ -0,0 +1,5 @@
+export * from "./cashout.use-case";
+export * from "./crash-round.use-case";
+export * from "./create-round.use-case";
+export * from "./place-bet.use-case";
+export * from "./start-round.use-case";
