ddfb2563489d8d5d439fd59add8c76f46d513263
Allan Prado <allanp.vba@gmail.com>
2026-04-01 14:22:25 -0300

    feat(games): register GameGateway in AppModule
    Add WebSocket gateway to module providers.

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -3,9 +3,11 @@ import { EventEmitterModule } from "@nestjs/event-emitter";
 import { EnvModule } from "./infrastructure/env/env.module";
 import { GameEngineModule } from "./infrastructure/game-engine/game-engine.module";
 import { GamesController } from "./presentation/controllers/games.controller";
+import { GameGateway } from "./presentation/gateways";
 
 @Module({
   imports: [EnvModule, EventEmitterModule.forRoot(), GameEngineModule],
-  controllers: [GamesController]
+  controllers: [GamesController],
+  providers: [GameGateway]
 })
 export class AppModule {}
