47662f7c8f3a7651534535b75a12ad4ff34d88de
Allan Prado <allanp.vba@gmail.com>
2026-04-01 13:44:50 -0300

    refactor(games): wire GameEngineModule in AppModule
    Import GameEngineModule directly in AppModule for a
    flatter, simpler module hierarchy.

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { EnvModule } from "./infrastructure/env/env.module";
+import { GameEngineModule } from "./infrastructure/game-engine/game-engine.module";
 import { GamesController } from "./presentation/controllers/games.controller";
 
 @Module({
-  imports: [EnvModule],
-  providers: [],
+  imports: [EnvModule, GameEngineModule],
   controllers: [GamesController]
 })
 export class AppModule {}
